c62a146a2245f84c53eb8a2f8c3aef77ec1b194b
Liang49 <liangzhang2014@gmail.com>
2019-05-12 19:46:14 -0400

    click
    handle weather changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import InputBar from "./InputBar.js";
+import Title from "./Title.js";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      City: "Brooklyn",
+      Country: "United States"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      City: "Queens"
+    });
+  }
   render() {
     return (
       <div>
-        <InputBar />
+        <Title />
+        <InputBar onClick={this.handleClick} />
       </div>
     );
   }
